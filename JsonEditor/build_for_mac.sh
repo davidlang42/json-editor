a57751b5e4909c21cc3177a18fe6dc9aae80be5f
David Lang <davidlang42@gmail.com>
2023-08-29 19:33:47 +1000

    Move build properties into project file

--- JsonEditor/build_for_mac.sh
+++ JsonEditor/build_for_mac.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-dotnet publish -f net7.0-maccatalyst -c Release -p:MtouchLink=SdkOnly -p:CreatePackage=true -p:EnableCodeSigning=true -p:EnablePackageSigning=true -p:CodesignKey="Developer ID Application: David Lang (45435R99CA)" -p:CodesignProvision="JsonEditor" -p:CodesignEntitlements="Platforms\MacCatalyst\Entitlements.plist" -p:PackageSigningKey="Developer ID Installer: David Lang (45435R99CA)" -p:UseHardenedRuntime=true
+set -e
+dotnet publish -f net7.0-maccatalyst -c Release
 PKG_FILE="`ls -1 ./bin/Release/net7.0-maccatalyst/publish/JsonEditor*.pkg | tail -n 1`"
 echo Press ENTER to submit to Apple: $PKG_FILE
 read
